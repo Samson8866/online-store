a6784dea3616ee1ecccc314419968f0d98b34c06
Samson Hill <samsonhill@Samsons-MacBook-Air.local>
2024-12-21 14:59:47 -0800

    Class 01

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar></Navbar>
+    <h1>Hello world from React</h1>
+    </div>
+  );
+}
+
+export default App;
